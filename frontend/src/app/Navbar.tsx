1789fca8c23fd05911edba5afd61c69bd4f97aa6
Allanpt <jatallantavares@gmail.com>
2024-09-19 09:46:35 -0300

    size adjustments

--- frontend/src/app/Navbar.tsx
+++ frontend/src/app/Navbar.tsx
@@ -8,10 +8,10 @@ const NavBar = () => {
   const pathName = usePathname();
 
   const getLinkClass = (path: string) => {
-    const bothStyle = "duration-200 font-bold text-base ";
+    const bothStyle = "duration-200 font-bold text-lg ";
     const resStyle =
       pathName?.includes(path)
-        ? "relative text-white after:absolute after:left-0 after:bottom-[-20px] after:w-full after:h-[2px] after:bg-white"
+        ? "relative text-white after:absolute after:left-0 after:bottom-[-28px] after:w-full after:h-[4px] after:bg-white"
         : "text-gray-400 hover:text-white";
 
     return bothStyle + resStyle;
@@ -20,13 +20,13 @@ const NavBar = () => {
   if (pathName === "/login") return;
 
   return (
-    <nav className="flex gap-10 bg-uninterTheme-600 p-5 box-border">
+    <nav className="flex items-center gap-10 bg-uninterTheme-600 p-5 box-border">
       <Link href="/home">
         <Image
           src="/uninter-logo-1.png"
           alt="Uninter Logo"
-          width={100}
-          height={21}
+          width={200}
+          height={41}
           priority
         />
       </Link>
